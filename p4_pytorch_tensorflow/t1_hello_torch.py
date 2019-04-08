2e0eff01558490575254c649952dde62ca556e82
explorer-ading <explorer.ading@yahoo.com>
2019-04-08 10:23:41 +0800

    hello pytorch and tensorflow.

---
+++ p4_pytorch_tensorflow/t1_hello_torch.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+## pytorch install 
+# pip install torch torchvision
+# torch.load(model_weights, map_location=torch.device('cpu'))
+
+import torch
+print(torch.__version__)
+
