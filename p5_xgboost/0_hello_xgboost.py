93dd25269b4a7931fe241a4bec7addc3c698798f
explorer-ading <explorer.ading@yahoo.com>
2019-04-08 10:32:38 +0800

    xgboost add.

---
+++ p5_xgboost/0_hello_xgboost.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import xgboost
+print(xgboost.__version__)
+
+
+# https://www.analyticsvidhya.com/blog/2016/03/complete-guide-parameter-tuning-xgboost-with-codes-python/
+
+
