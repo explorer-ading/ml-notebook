5b1960f9bc0f89dd9f01d3c323b9a27dde233630
explorer-ading <explorer.ading@yahoo.com>
2019-04-11 15:39:16 +0800

    hello keras.

---
+++ p6_keras_tensorflow/1_hello_keras.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# https://raw.githubusercontent.com/keras-team/keras/master/examples/
+
+import keras 
+import tensorflow as tf
+
+print("keras:", keras.__version__)
+print("tensorflow:", tf.__version__)
+
