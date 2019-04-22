2c5224cbd3c54a2cd6c2a797d0d29f32ab1a65c8
explorer-ading <explorer.ading@yahoo.com>
2019-04-22 10:30:09 +0800

    keras practice - mnist sample.

--- p6_keras_tensorflow/1_hello_keras.py
+++ p6_keras_tensorflow/1_hello_keras.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# https://raw.githubusercontent.com/keras-team/keras/master/examples/
 
 import keras 
 import tensorflow as tf
 
 print("keras:", keras.__version__)
 print("tensorflow:", tf.__version__)
+
+# initial install 
+# pip3 install tensorflow
+# pip3 install keras
+
+# upgrade
+# pip3 install --upgrade tensorflow
+# pip3 install --upgrade keras
+
 
---
+++ p6_keras_tensorflow/2_keras_mnist.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import warnings
+warnings.filterwarnings('ignore')
+
+import numpy as np
+np.random.seed(123) # for reproducibility
+
+from keras.models import Sequential
+from keras.layers import Flatten, MaxPool2D, Conv2D, Dense, Reshape, Dropout
+from keras.utils import np_utils
+from keras.datasets import mnist
+
+# Load pre-shuffled MNIST data into train and test sets
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train = X_train.reshape(X_train.shape[0], 28, 28, 1)
+X_test = X_test.reshape(X_test.shape[0], 28, 28, 1)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+Y_train = np_utils.to_categorical(y_train, 10)
+Y_test = np_utils.to_categorical(y_test, 10)
+
+# define the model architecture −
+model = Sequential()
+model.add(Conv2D(32, 3, 3, activation = 'relu', input_shape = (28,28,1)))
+model.add(Conv2D(32, 3, 3, activation = 'relu'))
+model.add(MaxPool2D(pool_size = (2,2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation = 'relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation = 'softmax'))
+
+# compile the specified model −
+model.compile(loss = 'categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+
+# fit the model using training data −
+model.fit(X_train, Y_train, batch_size = 32, epochs = 10, verbose = 1)
+
+
